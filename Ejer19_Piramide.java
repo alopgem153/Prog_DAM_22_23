cdf3340b1e75ca0b611076e90e63fa2778d9666f
alopgem153 <alopgem153@g.educaand.es>
2022-11-09 00:25:08 +0100

    -subida

--- Ejer19_Piramide.java
+++ Ejer19_Piramide.java
@@ -6,6 +6,7 @@ public class Ejer19_Piramide {
      * Realiza un programa que pinte una pirámide por pantalla. La altura
        se debe pedir por teclado. El carácter con el que se pinta la pirámide
     *  también se debe pedir por teclado.
+    Alfonso
     */
      
     public static void main(String[] args) {
