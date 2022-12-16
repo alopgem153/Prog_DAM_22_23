414258cae9557a206c844d706a0ec5052209ca36
alopgem153 <alopgem153@g.educaand.es>
2022-12-16 10:16:42 +0100

    Insertar y eliminar en array completo

--- ArraysT5/EjemploArraySimpleNumerico.java
+++ ArraysT5/EjemploArraySimpleNumerico.java
@@ -12,6 +12,7 @@ public class EjemploArraySimpleNumerico {
         //tendré que mostrar un menu con acciones que se deben realizar con el array
 
         int[] numeros = new int[10];
+        int[] copiaNumeros = null;
 
         boolean salir = false;
         do
@@ -56,9 +57,26 @@ public class EjemploArraySimpleNumerico {
                     break;
                 case 10:
                     desordenarTabla(numeros);
+                    break;
                 case 0:
                     salir = true;
                     sc.close();
+                    break;
+                case 11:
+                    copiaNumeros = copiarArray(numeros);
+                    imprimir(copiaNumeros);
+                    break;
+                case 12:
+                    imprimir(copiaNumeros);
+                    break;
+                case 13:
+                    numeros = insertarArrayCompleto(numeros);
+                    break;
+                case 14: 
+                    numeros = eliminarUltimoArrayCompleto(numeros);
+                    break;
+                case 15:
+                    numeros = insertarEnPosicionArrayCompleto(numeros);
                     break;
                 default:
                     System.out.println("Opción no Valida");
@@ -83,6 +101,13 @@ public class EjemploArraySimpleNumerico {
         System.out.println("7- Buscar un valor- devuelve la posición en la que esta -1 si no está");
         System.out.println("8- Ordenar");
         System.out.println("9- Busqueda en tabla ordenada");
+        System.out.println("10- Desordenar");
+        System.out.println("11- Copiar en CopiaNumero");
+        System.out.println("12- Imprimir CopiaNumero");
+        System.out.println("____________________________");
+        System.out.println("13- Insertar al final en array Completo (no ordenado)");
+        System.out.println("14- Eliminar ultima posicion en Completo (no ordenado");
+        System.out.println("15- Insertar en posicion de Array Completo(no ordenado)");
         System.out.println("0- Salir");
 
         System.out.println("____________________________");
@@ -233,7 +258,123 @@ public class EjemploArraySimpleNumerico {
      */
     static void desordenarTabla(int[] tabla)
     {
+        //recorrer toda la tabla/array
+        for(int aux = 0; aux< tabla.length; aux++)
+        {
+            //en cada posición genero un numero aleatorio de 0 a longitud-1
+            int nuevaPosicion = (int)(Math.random()*(tabla.length));
+
+            //intercambio la posicion aux por la posición nuevaPosicion
+            int valorNuevaPosicion = tabla[nuevaPosicion];
+            tabla[nuevaPosicion] = tabla[aux];
+            tabla[aux] = valorNuevaPosicion;
+        }
+    }
+
+
+    static int[] copiarArray(int[] original)
+    {
+
+        //Forma Manual
+
+        // int[] copia = new int[original.length];
+
+        // int indice = 0;
+        // for (int i : original) {
+        //     copia[indice] = i;
+        //     indice++;
+        // }
+
+        // return copia;
+
+        //Utilizado metodos de la clase Arrays
+        int[] copia = Arrays.copyOf(original, original.length);
+        return copia;
 
     }
+
+    /**
+     * Crea un nuevo array con una posición mas e inserta al final un nuevo valor
+     * @param original
+     * @return
+     */
+    static int[] insertarArrayCompleto(int[] original)
+    {
+        System.out.println("¿Que número quieres insertar?...");
+        int nuevoNumero = sc.nextInt();
+
+        //creo la copia del array original con una posición más
+        int[] copia = Arrays.copyOf(original, original.length+1);
+
+        //Inserto en la última posición del array copia el nuevo valor
+        copia[copia.length-1] = nuevoNumero;
+
+        return copia;
+    }
+
+    /**
+     * Elimina del array el ultimo elemento y elimina esta posición
+     * @param numeros
+     * @return
+     */
+    static int[] eliminarUltimoArrayCompleto(int[] numeros)
+    {
+        if(numeros.length >0)
+        {
+            int[] copia = Arrays.copyOf(numeros, numeros.length-1);
+            return copia;
+        }
+        else
+        {
+            System.out.println("El array está vacio");
+            return numeros;
+
+        }
+        
+    }
+
+
+    static int[] insertarEnPosicionArrayCompleto(int[] original)
+    {
+        //Este mismo algoritmo lo teneis en los apuntes utilizando el metodo Arrays.arrayCopy
+
+        System.out.println("¿que nuevo valor quieres insertar?...");
+        int nuevoNumero = sc.nextInt();
+        System.out.println("¿en que posición?...");
+        int posicionInsercion = sc.nextInt();
+
+        //necesito un array de longitud +1 que el original
+        int[] copia = new int[original.length+1];
+        //recorro todo el array original yvoy copiando sus valores 
+        int indiceCopia = 0;
+        for(int aux = 0; aux< original.length; aux++)
+        {
+            //hasta llegar a la posicion en la que quiero insertar el nuevo valor
+            if(aux != posicionInsercion)
+            {
+                copia[indiceCopia] = original[aux];
+                indiceCopia++;
+            }
+            else //estoy en la posicion de copia
+            {
+                //copio el nuevo valor en el nuevo array
+                copia[indiceCopia] = nuevoNumero;
+                indiceCopia++;
+                //y copio el el valor que estaba en esa posición 
+                //en una posicion mas adelantada
+                copia[indiceCopia] = original[aux];
+                indiceCopia++;
+            }
+
+            //inserto en nuevo valor
+            //sigo insertando los valores antiguos en el nuevo
+        }
+
+        return copia;
+
+        
+
+    }
+
 
 }
